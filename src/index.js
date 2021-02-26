eeb8931242ce408ba9719485f09d36d08828df92
enzo leonard <enzoleonard96@gmail.com>
2021-02-26 22:18:04 +0100

    template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import message from "../lib/hello"
-
+import faker from "faker"
 console.log(message);
+
+faker.locale = "fr";
+
+const randomName = faker.lorem.paragraphs(); // Rowan Nikolaus
+const randomWord = faker.random.words(); // Rowan Nikolaus
+
+console.log(randomName)
+console.log(randomWord)

5048cdda5a9abac6f1e1a213b8853a119a270357
enzo leonard <enzoleonard96@gmail.com>
2021-03-06 09:03:34 +0100

    update template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import message from "../lib/hello"
-import faker from "faker"
+
 console.log(message);
 
-faker.locale = "fr";
-
-const randomName = faker.lorem.paragraphs(); // Rowan Nikolaus
-const randomWord = faker.random.words(); // Rowan Nikolaus
-
-console.log(randomName)
-console.log(randomWord)
